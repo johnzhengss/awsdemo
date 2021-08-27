9a5fdb6e5e0c9fa907e8917ad29117152305e8ff
johnzhengss <john.zheng@smoothstack.com>
2021-08-26 19:06:02 -0500

    init

---
+++ src/main/java/com/ss/aws_demo_one/AwsDemoOneApplication.java
@@ -0,0 +1,13 @@
+package com.ss.aws_demo_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDemoOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsDemoOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ss/aws_demo_one/AwsDemoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.aws_demo_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
